ce24670c327e9fd5857d49aacbd845afedab8d87
pittbull <pbull809@gmail.com>
2015-03-12 18:52:48 +0100

    sicherheitskopie

--- misc.php
+++ misc.php
@@ -6,14 +6,52 @@ function math_eval($s)
     return $ma;
 }
 
+function conv ($in)
+{
+    return iconv ('UTF-8', 'windows-1252', $in);
+}
+
+function hcheader (FPDF $pdf, $xstart, $ystart)
+{
+    $y = $ystart; $x = $xstart;
+    $pdf->Line ($x, $y, $x+80, $y);
+    $y += 6;
+    $pdf->SetXY ($x,$y);
+    $pdf->Write(5, conv ('• Hardware'));
+    $y += 6;
+    $pdf->SetXY ($x,$y);
+    $pdf->Write(5, conv ('• Software'));
+    $y += 6;
+    $pdf->SetXY ($x,$y);
+    $pdf->Write(5, conv ('• Netzwerke'));
+    $y += 6;
+    $pdf->SetXY ($x,$y);
+    $pdf->Write(5, conv ('• Beratung'));
+    $y = $ystart; $x = $xstart+30;
+    $pdf->SetXY ($x,$y);
+    $pdf->Write(5, conv ('• Eigene Werkstatt'));
+    $y += 6;
+    $pdf->SetXY ($x,$y);
+    $pdf->Write(5, conv ('• Vor-Ort-Service'));
+    $y += 6;
+    $pdf->SetXY ($x,$y);
+    $pdf->Write(5, conv ('• 24h-Service'));
+    $y += 6;
+    $pdf->SetXY ($x,$y);
+    $pdf->Write(5, conv ('• An- und Verkauf'));
+    $y += 12; $x = $xstart;
+    $pdf->Line ($x, $y, $x+80, $y);
+}
+
 include ('fpdf.php'); 
 function testpdf() 
 {
     $pdf = new FPDF();
     $pdf->AddPage();
-    $pdf->SetFont('Arial', 'B', 16);
-    $pdf->Cell(40, 10, 'Hello World!');
-    $pdf->Output();
+    $pdf->SetFont('times','',12);
+    $pdf->SetFontSize(12);
+    hcheader ($pdf, 2, 2);
+    $pdf->Output('c:\\test.pdf','F');
 }
 
 /*
