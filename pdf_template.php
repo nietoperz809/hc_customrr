bccc0973f2d1fe32904daca45df297be085eea75
pittbull <pbull809@gmail.com>
2015-03-25 21:34:41 +0100

    sicherheitskopie

--- pdf_template.php
+++ pdf_template.php
@@ -22,6 +22,12 @@ function out ($pdf, $txt)
     $pdf->Write(5, conv ($txt));
 }
 
+function out2 ($x, $y, $pdf, $txt)
+{
+    $pdf->SetXY ($x,$y);
+    $pdf->Write(5, conv ($txt));
+}
+
 function hcheader ($pdf, $xstart, $ystart)
 {
     $pdf->SetFont('times','',12);
@@ -112,7 +118,9 @@ function invoice_number ($pdf, $x, $y, $num)
     out ($pdf, "Das Lieferdatum entspricht dem Rechnungsdatum");
 }
 
-function table_header ($pdf, $x, $y)
+//zaman yüksel 017684588474 
+
+function table_header_new ($pdf, $x, $y)
 {
     $pdf->SetFont('times','B',12);
     $pdf->SetXY ($x, $y);
@@ -126,13 +134,24 @@ function table_header ($pdf, $x, $y)
     $pdf->Line($x-5, $y+5, $x+170, $y+5); 
 }
 
-function table_sumfield ($pdf, $x, $y, $sum)
+function table_header_used ($pdf, $x, $y)
+{
+    $pdf->SetFont('times','B',12);
+    out2 ($x, $y, $pdf, "Anzahl           Artikel");
+    out2 ($x+120, $y, $pdf, "Einzelpreis      Gesamtpreis");
+    $pdf->SetLineWidth (0.5);
+    $pdf->SetDrawColor (0,0,0);
+    $pdf->Line($x-5, $y+5, $x+170, $y+5); 
+}
+
+function table_sumfield_new ($pdf, $x, $y, $sum)
 {
     $mwst = $sum*0.19;
     $pdf->SetLineWidth (0.1);
     $pdf->SetDrawColor (0,0,0);
     $pdf->Line($x, $y, $x+81, $y); 
     $pdf->SetFont('courier','B',12);
+    $x++;
     $pdf->SetXY ($x, $y);
     out ($pdf, " Netto-Gesamtbetrag =".format_price($sum));
     $y += 5;
@@ -146,6 +165,20 @@ function table_sumfield ($pdf, $x, $y, $sum)
     $pdf->Line($x, $y+1, $x+81, $y+1); 
 }
 
+function table_sumfield_used ($pdf, $x, $y, $sum)
+{
+    $pdf->SetLineWidth (0.1);
+    $pdf->SetDrawColor (0,0,0);
+    $pdf->Line($x, $y, $x+81, $y); 
+    $pdf->SetFont('courier','B',12);
+    $pdf->SetXY ($x+1, $y);
+    out ($pdf, " Netto-Gesamtbetrag =".format_price($sum));
+    $y += 5;
+    $pdf->Line($x, $y, $x+81, $y); 
+    $pdf->Line($x, $y+1, $x+81, $y+1); 
+}
+
+
 /**
  * Generates Table rows
  * @param FPDF $pdf PDF object
@@ -154,7 +187,7 @@ function table_sumfield ($pdf, $x, $y, $sum)
  * @param array $counts 
  * @param array $prices
  */
-function table_rows ($pdf, $x, $y, $inv_lines)
+function table_rows_new ($pdf, $x, $y, $inv_lines)
 {
     $pdf->SetFont('courier','',12);
     $xold = $x;
@@ -195,28 +228,63 @@ function table_rows ($pdf, $x, $y, $inv_lines)
         $x = $xold;
         $netto_sum += $netto_g;
     }
-    table_sumfield ($pdf, $x+84, $y, $netto_sum);
+    return array ($x, $y, $netto_sum);
 }
 
-function footer1 ($pdf, $x, $y)
+function table_rows_used ($pdf, $x, $y, $inv_lines)
 {
-    $pdf->SetFont('times','',10);
-    $pdf->SetXY ($x, $y);
-    out ($pdf, "Die Ware bleibt bis zu vollständigen Bezahlung unser Eigentum");
-    $pdf->SetXY ($x, $y+5);
-    out ($pdf, "Bankverbindung: Postbank Hannover, BLZ 250 100 30, Konto-Nr. 454382306");
-    $pdf->SetXY ($x, $y+10);
-    out ($pdf, "Ust.-ID DE186984567");
-    $pdf->SetXY ($x, $y+15);
-    out ($pdf, "Geschäftsführer: H. Erkan");
+    $pdf->SetFont('courier','',12);
+    $xold = $x;
+    $yold = $y;
+    $netto_sum = 0;
+    while (1)
+    {
+        $arr = mysqli_fetch_array($inv_lines);
+        if ($arr == NULL)
+        {
+            break;
+        }
+        $price = $arr['price'];
+        $text = $arr['text'];
+        $counts = $arr['items'];
+        $netto_e = $price;
+        $netto_g = $counts*$price;
+        $pdf->SetXY ($x, $y);
+        if ($counts != 0)
+            out ($pdf, $counts);
+        $x += 20;
+        $pdf->SetXY ($x, $y);
+        out ($pdf, $text);
+        $x += 62;
+        $x += 28;
+        $pdf->SetXY ($x, $y);
+        if ($counts != 0)
+            out ($pdf, format_price($netto_e));
+        $x += 28;
+        $pdf->SetXY ($x, $y);
+        if ($counts != 0)
+            out ($pdf, format_price($netto_g));
+        $y += 5;
+        $x = $xold;
+        $netto_sum += $netto_g;
+    }
+    return array ($x, $y, $netto_sum);
 }
 
 function footer2 ($pdf, $x, $y)
 {
     $pdf->SetFont('times','',10);
     $pdf->SetXY ($x, $y);
-    out ($pdf, "Die Lieferung unterliegt gemäß Paragraf 25a UStG der Differenzbesteuerung");
-    footer1 ($pdf, $x, $y+5);
+    out ($pdf, "IBAN:DE88250100300454382306 BIC:PBNKDEFF250");
+    $y+=5;
+    $pdf->SetXY ($x, $y);
+    out ($pdf, "Die Ware bleibt bis zu vollständigen Bezahlung unser Eigentum");
+    $pdf->SetXY ($x, $y+5);
+    out ($pdf, "Bankverbindung: Postbank Hannover, BLZ 250 100 30, Konto-Nr. 454382306");
+    $pdf->SetXY ($x, $y+10);
+    out ($pdf, "Ust.-ID DE186984567 Finanzamt Hannover Steuer-Nr.25/111/11645");
+    $pdf->SetXY ($x, $y+15);
+    out ($pdf, "Geschäftsführer: H. Erkan");    
 }
 
 function signatures ($pdf, $x, $y)
@@ -232,11 +300,19 @@ function signatures ($pdf, $x, $y)
     $pdf->Line($x+110, $y, $x+175, $y); 
 }
 
-function endtext ($pdf, $x, $y, $type1, $type2)
+/**
+ * 
+ * @param type $pdf PDF obj
+ * @param type $x xPos
+ * @param type $y yPos
+ * @param type $type1 gebraucht/nichtgebraucht
+ * @param type $type2 bar/unbar
+ */
+ function endtext ($pdf, $x, $y, $type1, $type2)
 {
     $pdf->SetFont('times','BI',10);
     $pdf->SetXY ($x, $y);
-    if ($type1 == TRUE)
+    if ($type1 == '0')
     {
         out ($pdf, "6 Monate Gewährleistung");
     }
@@ -247,7 +323,7 @@ function endtext ($pdf, $x, $y, $type1, $type2)
     $pdf->SetXY ($x, $y+5);
     out ($pdf, "Wir danken für Ihren Auftrag.");
     $pdf->SetXY ($x, $y+10);
-    if ($type2 == TRUE)
+    if ($type2 == '0')
     {
         out ($pdf, "Betrag bar erhalten.");
     }
@@ -255,6 +331,20 @@ function endtext ($pdf, $x, $y, $type1, $type2)
     {
         out ($pdf, "Zahlbar sofort ohne Abzug auf unser Konto.");
     }
+}
+
+function table_new ($pdf, $q_inv_lines)
+{
+    table_header_new ($pdf, 20, 140);
+    $ret = table_rows_new ($pdf, 25, 147, $q_inv_lines);
+    table_sumfield_new ($pdf, $ret[0]+84, $ret[1], $ret[2]);
+}
+
+function table_used ($pdf, $q_inv_lines)
+{
+    table_header_used ($pdf, 20, 140);
+    $ret = table_rows_used ($pdf, 25, 147, $q_inv_lines);
+    table_sumfield_used ($pdf, $ret[0]+84, $ret[1], $ret[2]);
 }
 
 /**
@@ -271,12 +361,23 @@ function create_pdf ($arr_invoice, $arr_customer, $q_inv_lines)
     address_field ($pdf, 15, 45, $arr_customer);
     $pdf->Image ('pix/hanse.png', 115, 25);
     set_date ($pdf, 150, 96);
-    invoice_number($pdf, 15, 120, $arr_invoice['code']);
-    table_header ($pdf, 20, 140);
-    table_rows ($pdf, 25, 147, $q_inv_lines);
+    invoice_number($pdf, 15, 120, $arr_invoice['code']);  // calculate invoice number
+    //print_r($arr_invoice);
+    //exit;
+    if ($arr_invoice['typ'] == 1) // neu
+    {
+        table_new ($pdf, $q_inv_lines);
+    }
+    else // gebraucht
+    {
+        table_used ($pdf, $q_inv_lines);
+    }
     footer2 ($pdf, 15, 250);
     signatures ($pdf, 15, 240);
-    endtext($pdf, 15, 215, FALSE, FALSE);
+//    print_r ($arr_invoice['typ']);
+//    print_r ($arr_invoice['payment']);
+//    exit;
+    endtext($pdf, 15, 215, $arr_invoice['typ'], $arr_invoice['payment']);
     $path1 = 'c:\\'.$filename.'.pdf';
     $pdf->Output($path1,'F');
     send_file($path1);
--- rechlist.php
+++ rechlist.php
@@ -24,7 +24,7 @@ include 'misc.php';
         header ("Location: index.php");
     }
     $result = get_invoice_by_customer ($link, $id);
-    echo "<form action='$self' method='post'><table border = '1'>";
+    echo "<form action='$self' method='post'><div align = 'center'><table border = '1'>";
     echo "<tr><td>Datum</td><td>Rechnungsnummer</td></tr>";
     while ($arr = mysqli_fetch_assoc($result))
     {
@@ -32,7 +32,7 @@ include 'misc.php';
         $code = $arr['code'];
         echo "<tr><td>$date</td><td><input type='submit' name='rcode' value='$code'></td></tr>";
     }
-    echo "</table>"
+    echo "</table></div>"
     . "<br/><input type='submit' name='end' value='abbruch'></form>"; 
     ?>
     </body>
